b0aa313a18fddff52fc6961c33bdfcd3b4cb56db
krzysztof.daniel <krzysztof.daniel@gmail.com>
2013-04-26 17:31:15 +0000

    Initial commit of tests.

---
+++ org.zend.webapi.tests/src/org/zend/webapi/test/core/progress/TestStatusCode.java
@@ -0,0 +1,32 @@
+package org.zend.webapi.test.core.progress;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.zend.webapi.core.progress.StatusCode;
+
+public class TestStatusCode {
+
+	@Test
+	public void testValidStatusCode() {
+		StatusCode code = StatusCode.byName("error");
+		assertNotNull(code);
+		assertNotSame(StatusCode.UNKNOWN, code);
+		assertSame(StatusCode.ERROR, code);
+	}
+
+	@Test
+	public void testUnknownStatusCode() {
+		StatusCode code = StatusCode.byName("1234");
+		assertNotNull(code);
+		assertSame(StatusCode.UNKNOWN, code);
+	}
+
+	@Test
+	public void testNullStatusCode() {
+		StatusCode code = StatusCode.byName(null);
+		assertNotNull(code);
+		assertSame(StatusCode.UNKNOWN, code);
+	}
+
+}
