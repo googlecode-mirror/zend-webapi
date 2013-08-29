258ecf6868b6cd8532199a4e1ada47424780ce03
wojciech.galanciak@gmail.com <wojciech.galanciak@gmail.com@d7478d52-4d7f-5e3d-54bf-36c424e6840b>
2013-08-29 09:40:55 +0000

    [development] Disable ssl certification

--- branches/10_1_maintenance/org.zend.webapi.core/src/org/zend/webapi/internal/core/connection/WebApiEngine.java
+++ branches/10_1_maintenance/org.zend.webapi.core/src/org/zend/webapi/internal/core/connection/WebApiEngine.java
@@ -1,10 +1,12 @@
 package org.zend.webapi.internal.core.connection;
 
 import org.restlet.engine.Engine;
+import org.restlet.ext.net.HttpClientHelper;
 
 public class WebApiEngine extends Engine {
 
 	public void registerDefaultConnectors() {
+		getRegisteredClients().add(new HttpClientHelper(null));
 		getRegisteredClients().add(new WebApiHttpClientHelper(null));
 		getRegisteredClients().add(
 				new org.restlet.engine.local.ClapClientHelper(null));
